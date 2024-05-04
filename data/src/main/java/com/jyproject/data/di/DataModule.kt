cf53f54560da2b48b8348ac0d31b0886a6f6db54
JunYeong0314 <96372487+JunYeong0314@users.noreply.github.com>
2024-05-04 22:39:26 +0900

    [Refactor] 기존 로그인방식 삭제

--- data/src/main/java/com/jyproject/data/di/DataModule.kt
+++ data/src/main/java/com/jyproject/data/di/DataModule.kt
@@ -1,11 +1,9 @@
 package com.jyproject.data.di
 
 import com.jyproject.data.features.auth.AuthRepositoryImpl
-import com.jyproject.data.features.auth.UserAuthRepositoryImpl
 import com.jyproject.data.features.login.KakaoLoginRepositoryImpl
 import com.jyproject.data.features.login.NaverLoginRepositoryImpl
 import com.jyproject.domain.features.auth.repository.AuthRepository
-import com.jyproject.domain.features.auth.UserAuthRepository
 import com.jyproject.domain.features.login.repository.KakaoLoginRepository
 import com.jyproject.domain.features.login.repository.NaverLoginRepository
 import dagger.Binds
@@ -23,10 +21,6 @@ abstract class DataModule {
     // Kakao Login
     @Binds
     abstract fun bindKakaoLoginRepository(kakaoLoginRepositoryImpl: KakaoLoginRepositoryImpl): KakaoLoginRepository
-
-    // User Auth
-    @Binds
-    abstract fun bindUserAuthRepository(userAuthRepositoryImpl: UserAuthRepositoryImpl): UserAuthRepository
 
     // User Check
     @Binds
--- domain/src/main/java/com/jyproject/domain/features/auth/UserAuthRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.jyproject.domain.features.auth
-
-import com.jyproject.domain.models.LoginState
-
-interface UserAuthRepository {
-    fun checkUser(userId: String, loginState: (LoginState) -> Unit)
-}
